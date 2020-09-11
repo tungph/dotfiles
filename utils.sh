2b303d3f303ab5c45050438522c477b648f59d7b
Shawn <shawn@cinnamon.is>
2020-09-12 06:34:26 +0700

    Initial commit

--- utils.sh
+++ utils.sh
@@ -30,10 +30,10 @@ install_diff_so_fancy() {
 
 install_vim_plug() {
   if command -v nvim >/dev/null 2>&2; then
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  else
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  else
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
 }
