4f0ba4513a7a120af296b87d8398bec7e86e20fe
Shawn <shawn@cinnamon.is>
2020-09-12 06:47:22 +0700

    Initial commit

--- utils.sh
+++ utils.sh
@@ -10,7 +10,7 @@ install() {
   COMMAND=$PACKAGE
   [ "$PACKAGE" = "trash-cli" ] && COMMAND="trash"
 
-  echo echo "=================== Installing $PACKAGE ==================="
+  echo ">>> Installing $PACKAGE..."
 
   if ! command -v "$COMMAND" >/dev/null 2>&2; then
     [ "$(uname)" = "Darwin" ] && brew install "$PACKAGE"
@@ -18,21 +18,23 @@ install() {
   else
     echo "$PACKAGE has already installed on your system"
   fi
+  echo ""
 }
 
 install_diff_so_fancy() {
   PACKAGE=diff-so-fancy
-  echo echo "=================== Installing $PACKAGE ==================="
+  echo ">>> Installing $PACKAGE..."
   if ! command -v "$PACKAGE" >/dev/null 2>&2; then
     [ -d "$LOCAL_DIR/$PACKAGE" ] || git clone https://github.com/so-fancy/diff-so-fancy.git "$LOCAL_DIR/$PACKAGE"
     git config --global core.pager "diff-so-fancy | less --tabs=2 -RFX"
   else
     echo "$PACKAGE has already installed on your system"
   fi
+  echo ""
 }
 
 install_vim_plug() {
-  echo "=================== Installing vim plug ==================="
+  echo ">>> Installing vim plug..."
   if command -v nvim >/dev/null 2>&2; then
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -40,4 +42,5 @@ install_vim_plug() {
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   fi
+  echo ""
 }
