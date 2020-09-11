236617798b5de246c8b3a70015b36b084b0a0271
Shawn <shawn@cinnamon.is>
2020-09-11 19:33:39 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ## Shawn's dotfiles
 ```shell script
-cd ~ && rm -rf .dotfiles .local/dotfiles .localrc; git clone https://github.com/tungph/dotfiles.git ~/.local/dotfiles && cd ~/.local/dotfiles && chmod u+x ./install.sh && ./install.sh; zsh
+cd ~ && rm -rvf .dotfiles .local/dotfiles .localrc; git clone https://github.com/tungph/dotfiles.git ~/.local/dotfiles && cd ~/.local/dotfiles && chmod u+x ./install.sh && ./install.sh; zsh
 ```
--- utils.sh
+++ utils.sh
@@ -17,7 +17,7 @@ install() {
 install_diff_so_fancy() {
   PACKAGE=diff-so-fancy
   if ! command -v "$PACKAGE" >/dev/null 2>&2; then
-    git clone https://github.com/so-fancy/diff-so-fancy.git "$LOCAL_DIR/$PACKAGE"
+    [ -d "$LOCAL_DIR/$PACKAGE" ] || git clone https://github.com/so-fancy/diff-so-fancy.git "$LOCAL_DIR/$PACKAGE"
     git config --global core.pager "diff-so-fancy | less --tabs=2 -RFX"
   else
     echo "$PACKAGE has already installed on your system"
