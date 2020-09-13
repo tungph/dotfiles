1866b4f1847f28a0b171028030787684aacab9c3
Shawn <shawn@cinnamon.is>
2020-09-13 14:52:59 +0700

    sync

--- vim/install.sh
+++ vim/install.sh
@@ -2,7 +2,8 @@
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p ~/.config/nvim/
+mkdir -p ~/.config/nvim/undodir
+
 ln -sf "$DOTFILES/vim/vimrc.symlink" ~/.config/nvim/init.vim
 nvim +'PlugInstall --sync' +qa
 nvim +'PlugUpdate' +qa
