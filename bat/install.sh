26eb33e228c15cd868f9435a2b12e6b503af3fef
Shawn <shawn@cinnamon.is>
2020-09-14 12:13:41 +0700

    sync

---
+++ bat/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$(uname)" = "Linux" ]; then
+  sudo apt install bat
+  
+  if ! command -v "bat" >/dev/null 2>&2; then
+    wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb
+    ./bat_0.15.4_amd64.deb
+    rm bat_0.15.4_amd64.deb
+  fi
+fi
