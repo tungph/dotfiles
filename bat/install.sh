77ee83b5dd247a1157501a2acd6ab3613ad57b22
Shawn <shawn@cinnamon.is>
2020-09-14 12:18:20 +0700

    sync

--- bat/install.sh
+++ bat/install.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" = "Linux" ]; then
   if ! command -v "bat" >/dev/null 2>&2; then
     wget https://github.com/sharkdp/bat/releases/download/v0.15.4/bat_0.15.4_amd64.deb
     chmod +x bat_0.15.4_amd64.deb 
-    ./bat_0.15.4_amd64.deb
+    sudo dpkg -i bat_0.15.4_amd64.deb
     rm bat_0.15.4_amd64.deb
   fi
 fi
