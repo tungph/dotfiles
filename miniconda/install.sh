c5c5367ed416022df9c0c5723b1d0ff10656eb65
Shawn <shawn@cinnamon.is>
2020-09-17 14:10:47 +0700

    sync

--- miniconda/install.sh
+++ miniconda/install.sh
@@ -2,6 +2,7 @@
 [ -d "$HOME/miniconda3" ] && return 0
 [ -d "$HOME/anaconda" ] && return 0
 
-echo "> [OPTIONAL] Install miniconda3"
+echo "> [OPTIONAL] To install miniconda3, run this:"
 INSTALLER=Miniconda3-latest-Linux-x86_64.sh
 echo "wget https://repo.anaconda.com/miniconda/$INSTALLER && chmod +x $INSTALLER && ./$INSTALLER"
+echo ""
