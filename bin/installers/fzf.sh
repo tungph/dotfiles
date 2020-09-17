df48b577f94b0b0a9219f0cf443aaab90f455123
Shawn <shawn@cinnamon.is>
2020-09-17 19:53:15 +0700

    sync

--- bin/installers/fzf.sh
+++ bin/installers/fzf.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-sudo apt install -qqy fzf
-
-if ! command -v fzf >/dev/null 2>&2; then
-    if ! [ -d ~/.local/fzf ]; then
-        git clone -q --depth 1 https://github.com/junegunn/fzf.git "$HOME/.local/fzf"
+if ! sudo apt install -qqy fzf; then
+    if ! command -v fzf >/dev/null 2>&2; then
+        if ! [ -d ~/.local/fzf ]; then
+            git clone -q --depth 1 https://github.com/junegunn/fzf.git "$HOME/.local/fzf"
+        fi
+        ~/.local/fzf/install
     fi
-    ~/.local/fzf/install
 fi
