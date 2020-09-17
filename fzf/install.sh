f703a4c463d0a9eb555d7cc72fb4b4b6fadb69d9
Shawn <shawn@cinnamon.is>
2020-09-17 14:53:03 +0700

    sync

--- fzf/install.sh
+++ fzf/install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# [ -d ~/.local/fzf ] && return 0
+[ -d ~/.local/fzf ] && exit 0
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.local/fzf
 # shellcheck disable=SC1090
 . ~/.local/fzf/install
-
-return 0
