ff55961d3e72abe86abbd30e939c4c6d91c257d9
Shawn <shawn@cinnamon.is>
2020-09-17 20:12:21 +0700

    sync

--- bin/installers/fzf.sh
+++ bin/installers/fzf.sh
@@ -1,6 +1,4 @@
-if ! command -v fzf >/dev/null 2>&2; then
-    if ! [ -d ~/.local/fzf ]; then
-        git clone -q --depth 1 https://github.com/junegunn/fzf.git "$HOME/.local/fzf"
-    fi
-    ~/.local/fzf/install
+if ! [ -d ~/.local/fzf ]; then
+    git clone -q --depth 1 https://github.com/junegunn/fzf.git "$HOME/.local/fzf"
 fi
+~/.local/fzf/install
