86cd5c04a880088c78ee6636008135698569ed0c
Shawn <shawn@cinnamon.is>
2020-09-17 14:09:17 +0700

    sync

--- miniconda/install.sh
+++ miniconda/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+[ -d "$HOME/miniconda3" ] && return 0
+[ -d "$HOME/anaconda" ] && return 0
 
-if ! [ -d "$HOME/miniconda3" ]; then
-  echo "saving Miniconda.sh to $HOME"
-  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh "$HOME/"
-  chmod +x "$HOME/Miniconda3-latest-Linux-x86_64.sh"
-fi
+echo "> [OPTIONAL] Install miniconda3"
+INSTALLER=Miniconda3-latest-Linux-x86_64.sh
+echo "wget https://repo.anaconda.com/miniconda/$INSTALLER && chmod +x $INSTALLER && ./$INSTALLER"
