abdd95076a63084003e5b2140ec6ed2488cac716
Shawn <shawn@cinnamon.is>
2020-09-18 08:35:55 +0700

    sync

--- bin/installers/conda.sh
+++ bin/installers/conda.sh
@@ -3,8 +3,6 @@
 [ -d "$HOME/miniconda3" ] && return 0
 [ -d "$HOME/anaconda" ] && return 0
 
-INSTALLER=Miniconda3-latest-Linux-x86_64.sh
-wget "https://repo.anaconda.com/miniconda/$INSTALLER"
-chmod +x "$INSTALLER"
-./$INSTALLER
-rm -f "$INSTALLER"
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p "$HOME/miniconda3"
+rm -f "$HOME/miniconda.sh"
