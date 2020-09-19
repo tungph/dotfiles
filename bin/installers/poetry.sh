d93b1427822530085e2fde68137c373c585fbbf9
Shawn <shawn@cinnamon.is>
2020-09-20 01:56:03 +0700

    sync

--- bin/installers/poetry.sh
+++ bin/installers/poetry.sh
@@ -2,4 +2,5 @@
 
 export POETRY_HOME="$HOME/.local/poetry"
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
+mkdir -p "$HOME/.zfunc"
 poetry completions zsh >~/.zfunc/_poetry
