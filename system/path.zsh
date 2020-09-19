cf83ee5f83a54a1e3d0d1dcf8b78f6c49f7d25ba
Shawn <shawn@cinnamon.is>
2020-09-19 08:23:55 +0700

    sync

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,5 @@
 #!/bin/sh
-export PATH="$PATH:$DOTFILES/bin:$HOME/.local/bin"
+export PATH="$PATH:$DOTFILES/bin"
+
+[ -d "$HOME/.local/bin" ] && PATH="$PATH:$HOME/.local/bin"
+[ -d "/Users/shawn/Library/Python/3.8/bin" ] && PATH="$PATH:/Users/shawn/Library/Python/3.8/bin"
