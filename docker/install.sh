1e96587b6243c029ce5ce3fc2f3f8b215120c29b
Shawn <shawn@cinnamon.is>
2020-11-05 13:25:40 +0700

    sync

--- docker/install.sh
+++ docker/install.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 mkdir -p "$HOME/.docker/completions"
 
-if command -v docker-compose >/dev/null 2>&1; then
-	curl -sL https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose \
-		-o "$HOME/.docker/completions/_docker-compose"
-fi
-if command -v docker-machine >/dev/null 2>&1; then
-	curl -sL https://raw.githubusercontent.com/docker/machine/master/contrib/completion/zsh/_docker-machine \
-		-o "$HOME/.docker/completions/_docker-machine"
-fi
-if command -v docker >/dev/null 2>&1; then
-	curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
-		-o "$HOME/.docker/completions/_docker"
-fi
+curl -sL https://raw.githubusercontent.com/docker/compose/master/contrib/completion/zsh/_docker-compose \
+    -o "$HOME/.docker/completions/_docker-compose"
+curl -sL https://raw.githubusercontent.com/docker/machine/master/contrib/completion/zsh/_docker-machine \
+    -o "$HOME/.docker/completions/_docker-machine"
+curl -sL https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker \
+    -o "$HOME/.docker/completions/_docker"
+
+sudo usermod -aG docker $USER
+
