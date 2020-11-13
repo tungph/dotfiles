1d859c7339d29a450a61697111b4d22c042245c4
Shawn <shawn@cinnamon.is>
2020-11-13 15:56:50 +0700

    sync

---
+++ ssh/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+SSH_HOME="$HOME/.ssh"
+AUTH_KEY="$SSH_HOME/authorized_keys"
+
+if ! [ -d "$SSH_HOME" ]; then
+  mkdir "$SSH_HOME"
+  chmod 700 "$SSH_HOME"
+
+  touch "$AUTH_KEY"
+  chmod 400 "$AUTH_KEY"
+  echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCnAzrcO8s2TIjgeX3AqkzZd7sT8Yx87hVsS4ZT+N2TKf3aZ9UwdXwgL9CXEvwXewLevDNV2hhiEdQj359iJj2/zwZLjRDcigwc4qnHUctFTCsauLkD+UFqVUZH516t6odwNf1jzdBaCE8BqxbX6+l8eNr9wDCvtWm4tZn5vasNY1Vuh99fxim+bz+VlrgqvXHH/LtIGeiTwJz6TkF/Bt7A0KNMtAZ9MiqQ6uPn/VcnwKHt4StrnVlarPqSjXrtZIRLL+kvwdlS0Tu3wwDlyGnzwkFtqIsA3rOVm5sxptYXi77q3l7p359cH0e3Mbsy2ZX6xbdcLwqk6AoaqsWG3p8OckTRj0QhUSWNlOjjzxjGUKLkK2t+wnJYDsWq68C6MQI4dE9hTieyvGO4upktrsgQqKA9ljRdz6oVF1Sm2FPfzevHkjO1LFrriffhzINXp/KqL3RhyHYtZCNBIB8RXK6Wuo9t9wDmOZkhKLdGQu2qtRKxPFg0lKgJP8NH3sbiwxOhPoUaSNSiLhFvGzs0C/7HHSFxZaMJ6nlJlip0vOM8jEeLyNbP5jJ39eyAQ205xGinHkbnPufAzJ/X9KbzrXamO7I6NKLrQVugqxhbv6/lXZ4dr9YDm3801gHcYvCYrUj796deJrZvVeCJDa8RQN/k8Lubk4rr1z8BqY58izlHUw== shawn@cinnamon.is" >> "$AUTH_KEY"
+fi
