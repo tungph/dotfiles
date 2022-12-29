18aed26c204f31d20b988d238c25044ce4233a54
Shawn <shawn.dev.vn@gmail.com>
2022-12-29 12:32:58 +0700

    sync

--- git/install.sh
+++ git/install.sh
@@ -1,24 +1,13 @@
 #!/bin/sh
 
-if [ -z "$(git config --global --get user.email)" ]; then
-  git config --global user.name "Shawn"
-  git config --global user.email "shawn.dev.vn@gmail.com"
-fi
-
 # Don't ask ssh password all the time
 if [ "$(uname -s)" = "Darwin" ]; then
-	git config --global credential.helper osxkeychain
+  git config --global credential.helper osxkeychain
 else
-	git config --global credential.helper cache
+  git config --global credential.helper cache
 fi
 
 # better diffs
 if command -v diff-so-fancy >/dev/null 2>&1; then
-	git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
-fi
-
-# use vscode as mergetool
-if command -v code >/dev/null 2>&1; then
-	git config --global merge.tool vscode
-	git config --global mergetool.vscode.cmd "code --wait $MERGED"
+  git config --global core.pager "diff-so-fancy | less --tabs=4 -RFX"
 fi
