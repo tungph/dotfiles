bb6c372e97f353bb2ece18251837a1e46ddd2fae
Shawn <shawn.dev.vn@gmail.com>
2023-05-22 01:38:49 +0700

    sync

--- vim/install.sh
+++ vim/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 curl -sLf https://spacevim.org/install.sh | bash
 
-cd $HOME/.SpaceVim && git checkout v1.9.0
+cd $HOME/.SpaceVim && git checkout v2.1.0
 cd $HOME/.SpaceVim/bundle/vimproc.vim && make
-
